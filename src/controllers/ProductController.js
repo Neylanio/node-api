e90c55ea6ba63a00d250572b6d3dfb87487cff7b
Neylanio <neylaniogomes@gmail.com>
2020-06-17 23:11:18 -0300

    Start CRUD Project

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -5,6 +5,36 @@ module.exports = {
     async index(request, response){
         const products = await Product.find();
         
-        return response.json(products);
-    }
+        return response.status(200).json(products);
+    },
+
+    async create(request, response){
+        const product = await Product.create(request.body);
+
+        return product ? response.status(200).json(product) : response.status(400).json({message: "Error"});
+    },
+
+    async update(request, response){
+
+        const { id } = request.params;
+        const { title, description, url } = request.body;
+ 
+        const product = await Product.find({ _id: id });
+
+        const productAux = {
+            title, description, url
+        }
+
+        console.log(productAux);
+
+        // await product.update(productAux);
+
+        return response.status(201).json(productAux);
+
+    },
+
+    delete(request, response){
+
+    },
+    
 };
--- src/routes.js
+++ src/routes.js
@@ -4,5 +4,8 @@ const routes = express.Router();
 const ProductController = require('./controllers/ProductController');
 
 routes.get('/products', ProductController.index);
+routes.post('/products', ProductController.create);
+routes.put('/products/:id', ProductController.update);
+routes.delete('/products/:id', ProductController.delete);
 
 module.exports = routes;
