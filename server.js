11cb48ce52aef4166c78dc14bfdaa6e465caa8c1
Neylanio <neylaniogomes@gmail.com>
2020-06-17 22:15:55 -0300

    Connection DataBase, create product model

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
 const app = express();
 
-app.get('/', (req, resp) => {
-    resp.send("Hello World");
+mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true });
+requireDir('./src/models');
+
+const Product = mongoose.model('Product');
+
+app.get('/', (request, response) => {
+    Product.create({
+        title: 'React Native',
+        description: 'Build native apps with React',
+        url: 'http://github.com/facebook/react-native'                     
+    });
+    return response.send("Hello Neylanio");
 });
 
 
