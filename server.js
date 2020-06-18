c6dafd80d83994b96fc8a99c1007941e8ca8fc53
Neylanio <neylaniogomes@gmail.com>
2020-06-17 22:33:06 -0300

    Restructure files

--- server.js
+++ server.js
@@ -7,16 +7,7 @@ const app = express();
 mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true });
 requireDir('./src/models');
 
-const Product = mongoose.model('Product');
-
-app.get('/', (request, response) => {
-    Product.create({
-        title: 'React Native',
-        description: 'Build native apps with React',
-        url: 'http://github.com/facebook/react-native'                     
-    });
-    return response.send("Hello Neylanio");
-});
-
+//Routes
+app.use('/api', require('./src/routes'));
 
 app.listen(3000);
